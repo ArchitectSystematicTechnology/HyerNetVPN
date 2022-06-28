614b3b30dd1a318173beb7e54e19b8396e37b69e
cyBerta <cyberta@riseup.net>
2022-06-28 16:54:04 +0200

    implement tun2torsocks and Java wrapper TorTunnel

---
+++ app/src/main/java/se/leap/bitmaskclient/tor/TorTunnel.java
@@ -0,0 +1,47 @@
+package se.leap.bitmaskclient.tor;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.ref.WeakReference;
+
+import tun2torsocks.TorSocksTunnel;
+
+public class TorTunnel {
+    private final TorSocksTunnel torSocksTunnel;
+    private final TunOutputStreamWriter packetWriter;
+
+    public static class TunOutputStreamWriter implements tun2torsocks.PacketFlow {
+        private final WeakReference<OutputStream> tunOutputStream;
+
+        public TunOutputStreamWriter(OutputStream outputStream) {
+            this.tunOutputStream = new WeakReference<>(outputStream);
+        }
+
+        @Override
+        public void writePacket(byte[] bytes) {
+            try {
+                tunOutputStream.get().write(bytes);
+            } catch (NullPointerException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public TorTunnel(OutputStream tunOutputstream) {
+        torSocksTunnel = new TorSocksTunnel();
+        packetWriter = new TunOutputStreamWriter(tunOutputstream);
+    }
+
+    public void start(String socksIP, long socksPort) {
+        torSocksTunnel.startSocks(packetWriter, socksIP, socksPort);
+    }
+
+    public void stop() {
+        torSocksTunnel.close();
+    }
+
+    public void inputPacket(byte[] data) {
+        torSocksTunnel.inputPacket(data);
+    }
+
+}
