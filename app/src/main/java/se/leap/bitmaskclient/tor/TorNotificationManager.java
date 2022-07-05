95267314805249c31e5944a6da4f51ff90ae459b
cyBerta <cyberta@riseup.net>
2022-07-05 02:25:31 +0200

    add action to stop tor from notification

--- app/src/main/java/se/leap/bitmaskclient/tor/TorNotificationManager.java
+++ app/src/main/java/se/leap/bitmaskclient/tor/TorNotificationManager.java
@@ -20,10 +20,16 @@ import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import org.torproject.jni.TorService;
 
 import se.leap.bitmaskclient.R;
 
@@ -54,10 +60,7 @@ public class TorNotificationManager {
         if (shouldDropNotification() && !forceShowing) {
             return;
         }
-        NotificationManager notificationManager = initNotificationManager(context);
-        if (notificationManager == null) {
-            return;
-        }
+        NotificationManagerCompat notificationManager = initNotificationManager(context);
         String torStatusText = TorStatusObservable.getStringForCurrentStatus(context);
         String torStatusLog = TorStatusObservable.getNotificationLog(context);
         int progress = TorStatusObservable.getBootstrapProgress();
@@ -75,7 +78,18 @@ public class TorNotificationManager {
         if (progress > 0) {
             notificationBuilder.setProgress(100, progress, false);
         }
+
+        NotificationCompat.Action.Builder actionBuilder = new NotificationCompat.Action.Builder(R.drawable.ic_menu_close_clear_cancel,
+                context.getString(R.string.vpn_button_turn_off), stopTorServiceIntent(context));
+        notificationBuilder.addAction(actionBuilder.build());
+
         notificationManager.notify(TOR_SERVICE_NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+    private PendingIntent stopTorServiceIntent(Context context) {
+        Intent stopTorServiceIntent = new Intent(context, TorService.class);
+        stopTorServiceIntent.setAction(TorService.ACTION_STOP);
+        return PendingIntent.getService(context, 0, stopTorServiceIntent, PendingIntent.FLAG_CANCEL_CURRENT);
     }
 
     private boolean shouldDropNotification() {
@@ -88,11 +102,8 @@ public class TorNotificationManager {
     }
 
 
-    private static NotificationManager initNotificationManager(Context context) {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        if (notificationManager == null) {
-            return null;
-        }
+    private static NotificationManagerCompat initNotificationManager(@NonNull Context context) {
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createNotificationChannel(context, notificationManager);
         }
@@ -100,7 +111,7 @@ public class TorNotificationManager {
     }
 
     @TargetApi(26)
-    private static void createNotificationChannel(Context context, NotificationManager notificationManager) {
+    private static void createNotificationChannel(Context context, NotificationManagerCompat notificationManager) {
         String appName = context.getString(R.string.app_name);
         CharSequence name =  context.getString(R.string.channel_name_tor_service, appName);
         String description = context.getString(R.string.channel_description_tor_service, appName);
