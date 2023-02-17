b3b615cc0762f834358764005d3efb7fbc3213c6
kwadronaut <kwadronaut@autistici.org>
2023-02-17 13:05:08 +0100

    api 30

--- scripts/installFastlane.sh
+++ scripts/installFastlane.sh
@@ -10,20 +10,23 @@ gem install fastlane
 ## ------------------------------------------------------
 ## --- Android Emulator 
 
-sdkmanager "platforms;android-31"
+sdkmanager "platforms;android-30"
 #
 # Install Android SDK emulator package
 echo y | sdkmanager "emulator"
 
 #echo y | sdkmanager "system-images;android-25;google_apis;x86_64"
 #echo y | sdkmanager "system-images;android-27;google_apis;x86"
-echo y | sdkmanager "system-images;android-31;google_apis;x86_64"
 #echo y | sdkmanager "system-images;android-28;google_apis;x86_64"
-
+echo y | sdkmanager "system-images;android-30;google_apis;x86_64"
 #echo y | sdkmanager "system-images;android-31;google_apis;x86_64"
+
 #
-echo no | avdmanager create avd --force --name testApi31 --abi google_apis/x86_64 --package 'system-images;android-31;google_apis;x86_64'
-echo no | avdmanager create avd --force --name testApiduet --abi google_apis/x86_64 --package 'system-images;android-31;google_apis;x86_64'
+#echo no | avdmanager create avd --force --name testApi31 --abi google_apis/x86_64 --package 'system-images;android-31;google_apis;x86_64'
+#echo no | avdmanager create avd --force --name testApiduet --abi google_apis/x86_64 --package 'system-images;android-31;google_apis;x86_64'
+
+echo no | avdmanager create avd --force --name testApi31 --abi google_apis/x86_64 --package 'system-images;android-30;google_apis;x86_64'
+echo no | avdmanager create avd --force --name testApiduet --abi google_apis/x86_64 --package 'system-images;android-30;google_apis;x86_64'
 
 #echo no | avdmanager create avd --force --name testApi28 --abi google_apis/x86_64 --package 'system-images;android-28;google_apis;x86_64'
 #echo no | avdmanager create avd --force --name testApiduet --abi google_apis/x86_64 --package 'system-images;android-28;google_apis;x86_64'
